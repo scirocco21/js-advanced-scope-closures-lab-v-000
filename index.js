71b2fe033579e63542fbbd7d0584f53042e04e40
Seb Gertz <sebgertz@gmail.com>
2018-05-30 17:26:43 +0000

    all tests passing

---
+++ index.js
@@ -0,0 +1,13 @@
+function produceDrivingRange(blockRange) {
+  return function rangeChecker(start, end) {
+    let distance = parseInt(end) - parseInt(start);
+    console.log(distance)
+    return ( blockRange >= distance ? `within range by ${ Math.abs(distance - blockRange)}` : `${ Math.abs(blockRange - distance)} blocks out of range`);
+  }
+}
+
+function produceTipCalculator(percentage) {
+  return function(bill) {
+    return bill * percentage;
+  }
+}
