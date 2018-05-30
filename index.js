9147d202019b41dddfcb01258b679542e583c532
Seb Gertz <sebgertz@gmail.com>
2018-05-30 17:32:32 +0000

    createDriver function added

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function produceTipCalculator(percentage) {
     return bill * percentage;
   }
 }
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
